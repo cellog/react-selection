cc76eee3908d794eae5d200974101326a230e623
Gregory Beaver <greg@chiaraquartet.net>
2016-06-07 14:06:25 -0500

    test createSelectRect

--- test/mouseMath.test.js
+++ test/mouseMath.test.js
@@ -2,11 +2,12 @@ import 'should'
 import mouseMath from '../src/mouseMath.js'
 
 describe("mouseMath", () => {
+  const e = {
+    pageX: 50,
+    pageY: 50
+  }
+
   describe("isClick", () => {
-    const e = {
-      pageX: 50,
-      pageY: 50
-    }
 
     it("should accept a valid value", () => {
       mouseMath.isClick(e, { x: 50, y: 50 }, 5).should.be.true
@@ -22,6 +23,26 @@ describe("mouseMath", () => {
       mouseMath.isClick(e, { x: 56, y: 50 }, 5).should.be.false("x is 56")
 
       mouseMath.isClick(e, { x: 50, y: 44 }, 5).should.be.false("y is 44")
+    })
+  })
+
+  describe("createSelectRect", () => {
+    mouseMath.createSelectRect(e, { x: 55, y: 55 }).should.eql({
+      bottom: 55,
+      left: 50,
+      right: 55,
+      top: 50,
+      x: 50,
+      y: 50
+    })
+
+    mouseMath.createSelectRect(e, { x: 45, y: 45 }).should.eql({
+      bottom: 50,
+      left: 45,
+      right: 50,
+      top: 45,
+      x: 50,
+      y: 50
     })
   })
 })
