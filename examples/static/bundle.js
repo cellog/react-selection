5d1f4c38b2e2ff84530e77945090f59b93645b2f
Gregory Beaver <greg@chiaraquartet.net>
2016-06-08 17:47:47 -0500

    fix removed exports

--- examples/static/bundle.js
+++ examples/static/bundle.js
@@ -150,6 +150,11 @@
 
 	'use strict';
 
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	exports.Debug = exports.Selectable = exports.Selection = undefined;
+
 	var _Selection = __webpack_require__(2);
 
 	var _Selection2 = _interopRequireDefault(_Selection);
@@ -158,9 +163,17 @@
 
 	var _Selectable2 = _interopRequireDefault(_Selectable);
 
+	var _debug = __webpack_require__(4);
+
+	var _debug2 = _interopRequireDefault(_debug);
+
 	function _interopRequireDefault(obj) {
 	  return obj && obj.__esModule ? obj : { default: obj };
 	}
+
+	exports.Selection = _Selection2.default;
+	exports.Selectable = _Selectable2.default;
+	exports.Debug = _debug2.default;
 
 /***/ },
 /* 2 */
