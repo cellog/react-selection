f6d1ae2aecf67e9d1f8fcc50a176d39ad9d4219c
Gregory Beaver <greg@chiaraquartet.net>
2016-06-09 15:14:31 -0500

    add polyfill for Array.fill

--- examples/static/bundle.js
+++ examples/static/bundle.js
@@ -146,6 +146,44 @@
 	  _react2.default.createElement(SelectableThing, { thing: 'foo', index: 3 })
 	), document.getElementById('example1'));
 
+	if (!Array.prototype.fill) {
+	  Array.prototype.fill = function (value) {
+
+	    // Steps 1-2.
+	    if (this == null) {
+	      throw new TypeError('this is null or not defined');
+	    }
+
+	    var O = Object(this);
+
+	    // Steps 3-5.
+	    var len = O.length >>> 0;
+
+	    // Steps 6-7.
+	    var start = arguments[1];
+	    var relativeStart = start >> 0;
+
+	    // Step 8.
+	    var k = relativeStart < 0 ? Math.max(len + relativeStart, 0) : Math.min(relativeStart, len);
+
+	    // Steps 9-10.
+	    var end = arguments[2];
+	    var relativeEnd = end === undefined ? len : end >> 0;
+
+	    // Step 11.
+	    var final = relativeEnd < 0 ? Math.max(len + relativeEnd, 0) : Math.min(relativeEnd, len);
+
+	    // Step 12.
+	    while (k < final) {
+	      O[k] = value;
+	      k++;
+	    }
+
+	    // Step 13.
+	    return O;
+	  };
+	}
+
 	var generateThing = function generateThing() {
 	  return _react2.default.createElement(SelectableThing, { thing: 'hi' + (arguments.length <= 1 ? undefined : arguments[1]), index: arguments.length <= 1 ? undefined : arguments[1], key: arguments.length <= 1 ? undefined : arguments[1] });
 	};
