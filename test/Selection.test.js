ed3ef77588f9cba627e016b4d15136841f5aaf9d
Gregory Beaver <greg@chiaraquartet.net>
2016-06-17 16:00:12 -0500

    another passing test

--- test/Selection.test.js
+++ test/Selection.test.js
@@ -1,6 +1,7 @@
 import 'should'
 import $ from 'teaspoon'
 import Selection from '../src/Selection.jsx'
+import Selectable from '../src/Selectable.jsx'
 import React, { Component, PropTypes } from 'react'
 
 describe("Selection", () => {
@@ -19,6 +20,22 @@ describe("Selection", () => {
       )
     }
   }
+  const Child = class B extends Component {
+    static displayName='B'
+    static propTypes = {
+      value: PropTypes.string,
+      id: PropTypes.number
+    }
+    render() {
+      return <div key={this.props.id}>{this.props.value}</div>
+    }
+  }
+  const options = {
+    value: (props) => props.value,
+    key: (props) => props.id,
+    cacheBounds: true
+  }
+  const SelectableChild = Selectable(Child, options)
   describe("higher-order container creation", () => {
     it("should make a component", () => {
       const Thing = Selection(Blah)
@@ -102,7 +119,54 @@ describe("Selection", () => {
         containerBounds: component.bounds
       })
     })
-    it("should call onSelectSlot if constantSelection is enabled")
+
+    it("should call onSelectSlot if constantSelect is enabled", () => {
+      const spy = sinon.spy()
+      stuff = $((
+        <Thing selectable constantSelect onSelectSlot={spy}>
+          <SelectableChild value="hi" key={1} />
+          <SelectableChild value="hi2" key={2} />
+          <SelectableChild value="hi3" key={3} />
+        </Thing>
+          )).render()
+      const children = stuff.find(SelectableChild)
+      component = stuff[0]
+      component.bounds = {hi:'hi'}
+      const selectable1 = children[0]
+      const selectable2 = children[1]
+      const selectable3 = children[2]
+
+      component.updateState(null, {
+        3: {node: selectable3},
+        1: {node: selectable1},
+        2: {node: selectable2}
+      }, {
+        3: 'hi3',
+        1: 'hi',
+        2: 'hi2'
+      })
+
+      expect(spy.called).to.be.true
+
+      spy.args[0].should.have.length(5)
+      spy.args[0][0].should.be.eql({
+        3: 'hi3',
+        1: 'hi',
+        2: 'hi2'
+      })
+      expect(spy.args[0][1]()).to.eql({
+        3: {node: selectable3},
+        1: {node: selectable1},
+        2: {node: selectable2}
+      })
+      expect(spy.args[0][2]).to.eql(['hi', 'hi2', 'hi3'])
+      expect(spy.args[0][3]()).to.eql([
+        {node: selectable1},
+        {node: selectable2},
+        {node: selectable3}
+      ])
+      expect(spy.args[0][4]).to.eql({hi: 'hi'})
+    })
     it("should not call onSelectSlot if constantSelection is disabled")
   })
 })
