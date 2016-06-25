ec2c6316dbf58a12568578e323a40b7ced4f0091
Gregory Beaver <greg@chiaraquartet.net>
2016-06-25 00:02:44 -0500

    fix eslint issues

--- src/shallowEqual.js
+++ src/shallowEqual.js
@@ -1,30 +1,30 @@
 export default function shallowEqual(objA, objB) {
   if (objA === objB) {
-    return true;
+    return true
   }
 
-  const keysA = Object.keys(objA);
-  const keysB = Object.keys(objB);
+  const keysA = Object.keys(objA)
+  const keysB = Object.keys(objB)
 
   if (keysA.length !== keysB.length) {
     return false;
   }
 
   // Test for A's keys different from B.
-  const hasOwn = Object.prototype.hasOwnProperty;
+  const hasOwn = Object.prototype.hasOwnProperty
   for (let i = 0; i < keysA.length; i++) {
     if (!hasOwn.call(objB, keysA[i]) ||
       objA[keysA[i]] !== objB[keysA[i]]) {
       return false;
     }
 
-    const valA = objA[keysA[i]];
-    const valB = objB[keysA[i]];
+    const valA = objA[keysA[i]]
+    const valB = objB[keysA[i]]
 
     if (valA !== valB) {
-      return false;
+      return false
     }
   }
 
-  return true;
+  return true
 }
--- src/shallowEqualScalar.js
+++ src/shallowEqualScalar.js
@@ -1,34 +1,34 @@
 export default function shallowEqualScalar(objA, objB) {
   if (objA === objB) {
-    return true;
+    return true
   }
 
   if (typeof objA !== 'object' || objA === null ||
     typeof objB !== 'object' || objB === null) {
-    return false;
+    return false
   }
 
-  const keysA = Object.keys(objA);
-  const keysB = Object.keys(objB);
+  const keysA = Object.keys(objA)
+  const keysB = Object.keys(objB)
 
   if (keysA.length !== keysB.length) {
-    return false;
+    return false
   }
 
   // Test for A's keys different from B.
-  const hasOwn = Object.prototype.hasOwnProperty;
+  const hasOwn = Object.prototype.hasOwnProperty
   for (let i = 0; i < keysA.length; i++) {
     if (!hasOwn.call(objB, keysA[i])) {
-      return false;
+      return false
     }
 
-    const valA = objA[keysA[i]];
-    const valB = objB[keysA[i]];
+    const valA = objA[keysA[i]]
+    const valB = objB[keysA[i]]
 
     if (valA !== valB || typeof valA === 'object' || typeof valB === 'object') {
-      return false;
+      return false
     }
   }
 
-  return true;
+  return true
 }
