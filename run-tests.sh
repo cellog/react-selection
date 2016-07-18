ad53b986be4c425a06b6826daa7e1d4be51f77c1
Gregory Beaver <greg@chiaraquartet.net>
2016-07-18 06:40:15 -0500

    debug coverage creation

--- run-tests.sh
+++ run-tests.sh
@@ -1,34 +1,39 @@
 #!/usr/bin/env bash
 export NODE_ENV=test
-./node_modules/karma/bin/karma start test/karma/karma.edge.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.ie10.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.ie11.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.iphone5s.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.iphone6.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.iphone6plus.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.android4.1.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.android4.2.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.android4.3.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.android4.4.conf.js
+mkdir coverage
+#./node_modules/karma/bin/karma start test/karma/karma.edge.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.ie10.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.ie11.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.iphone5s.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.iphone6.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.iphone6plus.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.android4.1.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.android4.2.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.android4.3.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.android4.4.conf.js
 #./node_modules/karma/bin/karma start test/karma/karma.android5.1.conf.js
 ./node_modules/karma/bin/karma start test/karma/karma.chrome43.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.chrome44.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.chrome45.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.chrome46.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.chrome47.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.chrome48.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.chrome49.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.chrome50.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.chrome50osx.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.firefox44.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.firefox45.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.firefox46.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.firefox46osx.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.opera.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.safari6.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.safari7.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.safari8.conf.js
-./node_modules/karma/bin/karma start test/karma/karma.safari9.conf.js
-ls -l ./coverage
+#./node_modules/karma/bin/karma start test/karma/karma.chrome44.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.chrome45.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.chrome46.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.chrome47.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.chrome48.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.chrome49.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.chrome50.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.chrome50osx.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.firefox44.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.firefox45.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.firefox46.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.firefox46osx.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.opera.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.safari6.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.safari7.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.safari8.conf.js
+#./node_modules/karma/bin/karma start test/karma/karma.safari9.conf.js
+data=$(ls -l)
+for entry in ${data}
+do
+  echo ${entry}
+done
 ./node_modules/.bin/lcov-result-merger './coverage/**/lcov.info' './coverage/lcov.info'
-./node_modules/codeclimate-test-reporter/bin/codeclimate.js < ./coverage/lcov.info
+#./node_modules/codeclimate-test-reporter/bin/codeclimate.js < ./coverage/lcov.info
